1d3bc80948d8d259c34a023743149771656d45f6
Ishan <punjishanrenu@gmail.com>
2025-05-25 18:46:03 +0530

    added class AreaCircle

--- AreaCircle.java
+++ AreaCircle.java
@@ -1,3 +1,12 @@
 public class AreaCircle {
+    public double radius;
+    public double Area(){
+        math.PI*radius*radius;
+    }
+    public static void main(String[] args){
+        radius=5;
+        Area(radius);
+
+    }
 
 }
